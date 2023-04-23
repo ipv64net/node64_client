2557f3475f3d266b6db7c73662e0289d0297a523
root <root@tester.home.local>
2023-04-23 19:20:03 +0000

    Initial

---
+++ functions.py
@@ -0,0 +1,59 @@
+from pythonping import ping
+import requests, json, time, dns 
+import dns.resolver
+
+version = "0.0.1"
+
+def report_version(node_secret):
+    url = 'https://ipv64.net/dims/report_node_status.php'
+    myobj = {'node_secret' : node_secret,'version':version}
+    try:
+        x = requests.post(url, data = myobj)
+    except:
+        print("")    
+
+def report_ipv4(node_secret):
+    url = 'https://ipv4.ipv64.net/dims/report_node_status.php'
+    myobj = {'node_secret' : node_secret}
+    try:
+        x = requests.post(url, data = myobj)
+    except:
+        print("Skip: IPv4 could not be resolved")
+    
+def report_ipv6(node_secret):
+    url = 'https://ipv6.ipv64.net/dims/report_node_status.php'
+    myobj = {'node_secret' : node_secret}
+    try:
+        x = requests.post(url, data = myobj)
+    except:
+        print("Skip: IPv6 could not be resolved")
+
+def icmp(icmp_dst,icmp_size,icmp_count,icmp_interval,icmp_timeout):
+    response_list = ping(icmp_dst, size=icmp_size, count=icmp_count, interval=icmp_interval, timeout=icmp_timeout)
+    if response_list.success():
+        rtt_avg = response_list.rtt_avg_ms
+        rtt_min = response_list.rtt_max_ms
+        rtt_max = response_list.rtt_min_ms
+        packet_loss = response_list.packet_loss
+        task_result = {"rtt_avg":rtt_avg,"rtt_min":rtt_min,"rtt_max":rtt_max,"packet_loss":packet_loss}
+    else:
+        packet_loss = response_list.packet_loss
+        task_result = {"error_msg":"timeout","packet_loss":packet_loss}
+    data = json.dumps(task_result)
+    return data
+        
+def dns_resolve(query,query_type):
+    result = dns.resolver.Resolver()
+    #result.nameservers = [nameserver]
+    try:
+        result = result.resolve(query,query_type)
+        response_time = round(result.response.time * 100,4)
+        records = []
+        for IPval in result:
+            records.append(IPval.to_text())
+        data = {"rrset":records,"latency":response_time,"error":"no"}
+    except:
+        print("Could not be resolved")
+        data = {"error":"Could not be resolved"}
+    data = json.dumps(data)
+    return data
---
+++ ipv64_client.py
@@ -0,0 +1,68 @@
+from pythonping import ping
+import requests, json, time, sys
+import functions
+
+node_id = "8g8893ff"
+node_secret = sys.argv[1]
+
+print("IPv64.net - Initialisierung")
+# At Start, report IPv4 and IPv6
+functions.report_ipv4(node_secret)
+functions.report_ipv6(node_secret)
+functions.report_version(node_secret)
+
+while(True):
+    url = 'https://ipv64.net/dims/get_task.php'
+    myobj = {'node_secret' : node_secret}
+
+    x = requests.post(url, data = myobj)
+
+    x = x.json()
+    if x["report_ip"] == '1':
+        functions.report_ipv4(node_secret)
+        functions.report_ipv6(node_secret)
+        print("Report IPv4 & IPv6")
+    if 'tasks' in x:
+        for _tasks in x["tasks"]:
+            task_result = None
+            if(_tasks["task_type"] == "icmpv4"):
+                print("Start ICMPv4 Task ID: "+_tasks["task_id"])
+                icmp_dst = _tasks["task_infos"]["icmp_dst"]
+                icmp_size = _tasks["task_infos"]["icmp_size"]
+                icmp_count = _tasks["task_infos"]["icmp_count"]
+                icmp_interval = _tasks["task_infos"]["icmp_interval"]
+                icmp_timeout = _tasks["task_infos"]["icmp_timeout"]
+                
+                task_result = functions.icmp(icmp_dst,icmp_size,icmp_count,icmp_interval,icmp_timeout)
+                print(task_result)
+                print("End ICMPv4 Task ID: "+_tasks["task_id"])
+                
+            if(_tasks["task_type"] == "icmpv6"):
+                print("Start ICMPv6 Task ID: "+_tasks["task_id"])
+                icmp_dst = _tasks["task_infos"]["icmp_dst"]
+                icmp_size = _tasks["task_infos"]["icmp_size"]
+                icmp_count = _tasks["task_infos"]["icmp_count"]
+                icmp_interval = _tasks["task_infos"]["icmp_interval"]
+                icmp_timeout = _tasks["task_infos"]["icmp_timeout"]
+                
+                task_result = functions.icmp(icmp_dst,icmp_size,icmp_count,icmp_interval,icmp_timeout)
+                print(task_result)
+                print("End ICMPv6 Task ID: "+_tasks["task_id"])
+                
+            if(_tasks["task_type"] == "dns"):
+                print("Start DNS Task ID: "+_tasks["task_id"])
+                dns_query = _tasks["task_infos"]["dns_query"]
+                dns_type = _tasks["task_infos"]["dns_type"]
+                
+                task_result = functions.dns_resolve(dns_query,dns_type)
+                print(task_result)
+                print("End DNS Task ID: "+_tasks["task_id"])
+                
+            if task_result is not None:            
+                url = 'https://ipv64.net/dims/task_report_result.php'
+                myobj = {'node_secret' : node_secret,'task_id':_tasks["task_id"],'task_result':task_result}
+                req = requests.post(url, data = myobj)
+    print("Wait for next Job.")
+    time.sleep(x["wait"])
+    
+print(f"Response: {x.json()}")
