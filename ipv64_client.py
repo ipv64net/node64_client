2594ff301091264bb0cee4359de2836b4e94ac99
Jonathan Nebel <jonathan.nebel@msg.group>
2023-04-24 10:26:51 +0200

    Reformat Files and disable SSL-Verification

--- ipv64_client.py
+++ ipv64_client.py
@@ -1,5 +1,9 @@
-import requests, json, time, sys
+import requests, time, sys
 import functions
+import urllib3
+
+#Hide verification message
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 node_secret = sys.argv[1]
 
@@ -9,11 +13,11 @@ functions.report_ipv4(node_secret)
 functions.report_ipv6(node_secret)
 functions.report_version(node_secret)
 
-while(True):
+while True:
     url = 'https://ipv64.net/dims/get_task.php'
-    myobj = {'node_secret' : node_secret}
+    myobj = {'node_secret': node_secret}
 
-    x = requests.post(url, data = myobj)
+    x = requests.post(url, data=myobj, verify=False)
 
     x = x.json()
     if x["error"] == '1':
@@ -26,44 +30,44 @@ while(True):
     if 'tasks' in x:
         for _tasks in x["tasks"]:
             task_result = None
-            if(_tasks["task_type"] == "icmpv4"):
-                print("Start ICMPv4 Task ID: "+_tasks["task_id"])
+            if _tasks["task_type"] == "icmpv4":
+                print("Start ICMPv4 Task ID: " + _tasks["task_id"])
                 icmp_dst = _tasks["task_infos"]["icmp_dst"]
                 icmp_size = _tasks["task_infos"]["icmp_size"]
                 icmp_count = _tasks["task_infos"]["icmp_count"]
                 icmp_interval = _tasks["task_infos"]["icmp_interval"]
                 icmp_timeout = _tasks["task_infos"]["icmp_timeout"]
-                
-                task_result = functions.icmp(icmp_dst,icmp_size,icmp_count,icmp_interval,icmp_timeout,4)
+
+                task_result = functions.icmp(icmp_dst, icmp_size, icmp_count, icmp_interval, icmp_timeout, 4)
                 print(task_result)
-                print("End ICMPv4 Task ID: "+_tasks["task_id"])
-                
-            if(_tasks["task_type"] == "icmpv6"):
-                print("Start ICMPv6 Task ID: "+_tasks["task_id"])
+                print("End ICMPv4 Task ID: " + _tasks["task_id"])
+
+            if _tasks["task_type"] == "icmpv6":
+                print("Start ICMPv6 Task ID: " + _tasks["task_id"])
                 icmp_dst = _tasks["task_infos"]["icmp_dst"]
                 icmp_size = _tasks["task_infos"]["icmp_size"]
                 icmp_count = _tasks["task_infos"]["icmp_count"]
                 icmp_interval = _tasks["task_infos"]["icmp_interval"]
                 icmp_timeout = _tasks["task_infos"]["icmp_timeout"]
-                
-                task_result = functions.icmp(icmp_dst,icmp_size,icmp_count,icmp_interval,icmp_timeout,6)
+
+                task_result = functions.icmp(icmp_dst, icmp_size, icmp_count, icmp_interval, icmp_timeout, 6)
                 print(task_result)
-                print("End ICMPv6 Task ID: "+_tasks["task_id"])
-                
-            if(_tasks["task_type"] == "dns"):
-                print("Start DNS Task ID: "+_tasks["task_id"])
+                print("End ICMPv6 Task ID: " + _tasks["task_id"])
+
+            if _tasks["task_type"] == "dns":
+                print("Start DNS Task ID: " + _tasks["task_id"])
                 dns_query = _tasks["task_infos"]["dns_query"]
                 dns_type = _tasks["task_infos"]["dns_type"]
-                
-                task_result = functions.dns_resolve(dns_query,dns_type)
+
+                task_result = functions.dns_resolve(dns_query, dns_type)
                 print(task_result)
-                print("End DNS Task ID: "+_tasks["task_id"])
-                
-            if task_result is not None:            
+                print("End DNS Task ID: " + _tasks["task_id"])
+
+            if task_result is not None:
                 url = 'https://ipv64.net/dims/task_report_result.php'
-                myobj = {'node_secret' : node_secret,'task_id':_tasks["task_id"],'task_result':task_result}
-                req = requests.post(url, data = myobj)
+                myobj = {'node_secret': node_secret, 'task_id': _tasks["task_id"], 'task_result': task_result}
+                req = requests.post(url, data=myobj, verify=False)
     print("Wait for next Job.")
     time.sleep(x["wait"])
-    
+
 print(f"Response: {x.json()}")
