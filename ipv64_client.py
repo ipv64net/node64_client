ce38bf29b2b432d7dc72d6d06fdf3c32ef697dfe
ipv64net <131612122+ipv64net@users.noreply.github.com>
2023-05-03 15:41:59 +0200

    Update ipv64_client.py

--- ipv64_client.py
+++ ipv64_client.py
@@ -42,7 +42,7 @@ while True:
     x = x.json()
     if x["verbose"] == '1':
         print(x)
-    if x["error"] > '0':
+    if x["error"] > 0:
         print("Error Code:"+ x["error"])
         break
     if x["report_ip"] == '1':
