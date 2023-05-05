b42b1f96fb16a1a38014b7ae922a07898c9ec426
Mr.  Phil <mr.phil1@gmx.net>
2023-05-05 18:35:11 +0200

    edit install-service.sh * add a help message * add an update function * use switch for the install, update and help functions

--- devices/Debian11/install-service.sh
+++ devices/Debian11/install-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-clear
+#clear
 ######here all the variables#######################################################################################
 apps="git build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev python3 python3-pip" #add here your application
 service_file="/etc/systemd/system/node64_client.service"
@@ -19,6 +19,12 @@ function get_ipv64_client() {
     cd /opt/
     git clone https://github.com/ipv64net/ipv64_client.git
     cd ipv64_client/
+    pip3 install -r requirements.txt
+}
+
+function update_ipv64_client() {
+    cd /opt/ipv64_client/
+    git pull
     pip3 install -r requirements.txt
 }
 
@@ -56,9 +62,70 @@ function make_initD_file() {
     echo "    procd_close_instance" >>"${initD_file}"
     echo "}" >>"${initD_file}"
 }
-get_ipv64_client
-ask_for_secret
-make_service_file
-make_initD_file
-echo "Enable the service with:"
-echo "systemctl enable --now node64_client"
+
+display_help() {
+    echo "Usage: $(basename "$0") [-h | --help] [-i | --install] [-u | --update] -- Here should be a description about the node64_client in the near future." >&2
+    echo
+    echo "  -h | --help    -> show this help text"
+    echo "  -i | --install -> install the node64_client on your system"
+    echo "  -u | --update  -> update the node64_client to the latest Version"
+    echo
+    # echo some stuff here for the -a or --add-options
+    exit 1
+}
+
+################################
+# Check if parameters options  #
+# are given on the commandline #
+################################
+while :; do
+    case "$1" in
+    -h | --help)
+        display_help # Call your function
+        exit 0
+        ;;
+    -i | --install)
+        echo "------------------------"
+        echo "Install the node64_Client"
+        echo "------------------------"
+        get_ipv64_client
+        ask_for_secret
+        make_service_file
+        make_initD_file
+        echo "------------------------"
+        echo "The installation of the node64_client is finished"
+        echo "------------------------"
+        echo "Enable the service with:"
+        echo "systemctl daemon-reload"
+        echo "systemctl enable node64_client.service"
+        echo "systemctl enable --now node64_client"
+        break
+        ;;
+    -u | --update)
+        echo "------------------------"
+        echo "Update the node64_Client"
+        echo "------------------------"
+        update_ipv64_client
+        echo "The update of the node64_client is finished"
+        echo "------------------------"
+        echo "now restart the node64_client Service with:"
+        echo "systemctl restart node64_client.service"
+        break
+        ;;
+
+    --) # End of all options
+        shift
+        break
+        ;;
+    -*)
+        echo "Error: Unknown option: $1" >&2
+        ## or call function display_help
+        display_help
+        exit 1
+        ;;
+    *) # No more options
+        display_help
+        exit 1
+        ;;
+    esac
+done
