1390a0f017a3ac1287d855f1262d12935cd6b652
Mr.  Phil <mr.phil1@gmx.net>
2023-05-06 18:49:00 +0200

    edit install-service.sh * change from node643_client to node64.io * change text style

--- devices/Debian11/install-service.sh
+++ devices/Debian11/install-service.sh
@@ -2,8 +2,8 @@
 clear
 ######here all the variables#######################################################################################
 apps="git build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev python3 python3-pip" #add here your application
-service_file="/etc/systemd/system/node64_client.service"
-initD_file="/etc/init.d/node64_client"
+service_file="/etc/systemd/system/node64_io.service"
+initD_file="/etc/init.d/node64_io"
 ####################################################################################################################
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root"
@@ -17,16 +17,15 @@ if ! dpkg -s $apps >/dev/null 2>&1; then
 fi
 
 function print_text() {
-    echo "______________________________________________________________________________________________________________"
-    echo '                        888           .d8888b.      d8888                    888 d8b                   888    '
-    echo '                        888          d88P  Y88b    d8P888                    888 Y8P                   888    '
-    echo '                        888          888          d8P 888                    888                       888    '
-    echo ' 88888b.   .d88b.   .d88888  .d88b.  888d888b.   d8P  888            .d8888b 888 888  .d88b.  88888b.  888888 '
-    echo ' 888 "88b d88""88b d88" 888 d8P  Y8b 888P "Y88b d88   888           d88P"    888 888 d8P  Y8b 888 "88b 888    '
-    echo ' 888  888 888  888 888  888 88888888 888    888 8888888888          888      888 888 88888888 888  888 888    '
-    echo ' 888  888 Y88..88P Y88b 888 Y8b.     Y88b  d88P       888           Y88b.    888 888 Y8b.     888  888 Y88b.  '
-    echo ' 888  888  "Y88P"   "Y88888  "Y8888   "Y8888P"        888  88888888  "Y8888P 888 888  "Y8888  888  888  "Y888 '
-    echo "______________________________________________________________________________________________________________"
+    echo "______________________________________________________"
+    echo "                   _         ____    ___     _        "
+    echo "                  | |       / ___|  /   |   (_)       "
+    echo " _ __    ___    __| |  ___ / /___  / /| |    _   ___  "
+    echo "| '_ \  / _ \  / _\` | / _ \| ___ \/ /_| |   | | / _ \ "
+    echo "| | | || (_) || (_| ||  __/| \_/ |\___  | _ | || (_) |"
+    echo "|_| |_| \___/  \__,_| \___|\_____/    |_/(_)|_| \___/ "
+    echo "______________________________________________________"
+    # @uelle: https://patorjk.com/software/taag/#p=display&h=1&v=2&f=Doom&t=node64.io%0A
 }
 
 function get_ipv64_client() {
@@ -82,11 +81,11 @@ function make_initD_file() {
 
 display_help() {
     echo "Usage: install-service.sh [-h | --help] [-i | --install] [-u | --update]"
-    echo "Node64 worker client that is receiving tasks for dns, icmp and tracroute task."
+    echo "The Node64.io client is receiving tasks for dns, icmp and tracroute task."
     echo
     echo "  -h | --help    -> show this help text"
-    echo "  -i | --install -> install the node64_client on your system"
-    echo "  -u | --update  -> update the node64_client to the latest Version"
+    echo "  -i | --install -> install the node64_io on your system"
+    echo "  -u | --update  -> update the node64_io to the latest Version"
     echo
     exit 1
 }
@@ -101,27 +100,27 @@ while :; do
     -i | --install)
         if [ ! -d "/opt/ipv64_client/.git" ]; then
             print_text
-            echo "------------------------"
-            echo "Install the node64_Client"
-            echo "------------------------"
+            echo "****************************"
+            echo "Install the node64_io Client"
+            echo "****************************"
             get_ipv64_client
             ask_for_secret
             make_service_file
             make_initD_file
-            echo "------------------------"
-            echo "The installation of the node64_client is finished"
-            echo "------------------------"
+            echo "----------------------------------------------------"
+            echo "The installation of the node64_io Client is finished"
+            echo "----------------------------------------------------"
             echo "Enable the service with:"
             echo "sudo systemctl daemon-reload"
-            echo "sudo systemctl enable node64_client.service"
-            echo "sudo systemctl start node64_client.service"
+            echo "sudo systemctl enable node64_io.service"
+            echo "sudo systemctl start node64_io.service"
             break
         else
             print_text
-            echo "***********************************"
-            echo "The node64_client is already installed on your System."
-            echo "Please update node64_client via -u."
-            echo "***********************************"
+            echo "*********************************************************"
+            echo "The node64_io Client is already installed on your System."
+            echo "Please update node64_io Client via -u."
+            echo "*********************************************************"
             display_help
             exit 1
         fi
@@ -129,21 +128,22 @@ while :; do
     -u | --update)
         if [ -d "/opt/ipv64_client/.git" ]; then
             print_text
-            echo "------------------------"
-            echo "Update the node64_Client"
-            echo "------------------------"
+            echo "***************************"
+            echo "Update the node64_io Client"
+            echo "***************************"
             update_ipv64_client
-            echo "The update of the node64_client is finished"
-            echo "------------------------"
+            echo "----------------------------------------------"
+            echo "The update of the node64_io Client is finished"
+            echo "----------------------------------------------"
             echo "now restart the node64_client Service with:"
-            echo "systemctl restart node64_client.service"
+            echo "systemctl restart node64_io.service"
             break
         else
             print_text
-            echo "***********************************"
-            echo "The directory has not been cloned from Github or node64_client has not been installed yet."
-            echo "Please install node64_client via github with -i."
-            echo "***********************************"
+            echo "*********************************************************************************************"
+            echo "The directory has not been cloned from Github or node64_io Client has not been installed yet."
+            echo "Please install node64_io Client via github with -i."
+            echo "*********************************************************************************************"
             display_help
             exit 1
         fi
