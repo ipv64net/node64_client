ccc14355b5feb2cb65a07fd1da610ba0dd894a73
The_Think3r <thethink3r@gblan.de>
2023-05-26 22:18:21 +0200

    add example

--- example/Own/node64.py
+++ example/Own/node64.py
@@ -10,10 +10,16 @@ elif len(sys.argv) == 1:
 else:
     node_secret = sys.argv[1]
 
+class myclient(Node64Client):
+    def stats(self,_,task,result,response,runtime):
+        print(f"task {task}")
+        print(f"result {result}")
+        print(f"response {response}")
+        print(f"runtime {runtime}")
 
 if __name__ == "__main__":
     if geteuid() != 0:
         exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
     else:
-        client = Node64Client(node_secret)
+        client = myclient(node_secret)
         client.run()
---
+++ example/Prometheus/node64.py
@@ -0,0 +1,44 @@
+from Node64Client import Node64Client
+from os import geteuid, getenv
+import sys
+
+if getenv('Node64Secret'):
+    node_secret = getenv('Node64Secret')
+elif len(sys.argv) == 1:
+    print(f"{sys.argv[0]} <your node secret>")
+    sys.exit(1)
+else:
+    node_secret = sys.argv[1]
+
+class myclient(Node64Client):
+    def stats(self,_,task,result,response,runtime):
+        print(f"task {task}")
+        print(f"result {result}")
+        print(f"response {response}")
+        print(f"runtime {runtime}")
+
+###### TODO Jonathan #########
+###### TODO Jonathan #########
+###### TODO Jonathan #########
+###### TODO Jonathan #########
+###### TODO Jonathan #########
+###### TODO Jonathan #########
+###### TODO Jonathan #########
+###### TODO Jonathan #########
+###### TODO Jonathan #########
+###### TODO Jonathan #########
+###### TODO Jonathan #########
+###### TODO Jonathan #########
+###### TODO Jonathan #########
+###### TODO Jonathan #########
+###### TODO Jonathan #########
+###### TODO Jonathan #########
+
+
+
+if __name__ == "__main__":
+    if geteuid() != 0:
+        exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
+    else:
+        client = myclient(node_secret)
+        client.run()
