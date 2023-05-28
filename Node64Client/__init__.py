cf7651c390389e39f0290690817ffa36b870c226
The_Think3r <thethink3r@gblan.de>
2023-05-28 19:01:00 +0200

    fix Color Disable

--- Node64Client/__init__.py
+++ Node64Client/__init__.py
@@ -43,7 +43,7 @@ class Node64Client:
 
     def __init__(self,SecretKey,colorOutput = False):
         self.SecretKey = SecretKey
-        if colorOutput:
+        if not colorOutput:
             self.CINFO = self.CDEBUG = self.COK = self.CWARNING = self.CERROR = self.ENDC = ''  
         self.printInfo(f"SecretKey: {self.SecretKey}")
         self.report_ipv4()
