c3f0b93a5e0b82f60884c08010ba56b7f520bc32
The_Think3r <thethink3r@gblan.de>
2023-05-28 19:23:45 +0200

    Add Color Env Option

--- node64.py
+++ node64.py
@@ -2,18 +2,22 @@ from Node64Client import Node64Client
 from os import geteuid, getenv
 import sys
 
-if getenv('Node64Secret'):
-    node_secret = getenv('Node64Secret')
-elif len(sys.argv) == 1:
+nodeColor = False # Default Disable
+if str(getenv('Node64Color')).lower() == "true":
+    nodeColor=True
+
+if len(sys.argv) == 2:
+    nodeSecret = sys.argv[1]
+elif getenv('Node64Secret'):
+    nodeSecret = getenv('Node64Secret')
+else:
     print(f"{sys.argv[0]} <your node secret>")
     sys.exit(1)
-else:
-    node_secret = sys.argv[1]
-
+    
 
 if __name__ == "__main__":
     if geteuid() != 0:
         exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
     else:
-        client = Node64Client(node_secret)
+        client = Node64Client(nodeSecret,nodeColor)
         client.run()
