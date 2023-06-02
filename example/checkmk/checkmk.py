223e34e51e4ed966143ac9721fa10c024d6a465a
The_Think3r <thethink3r@gblan.de>
2023-06-02 20:15:40 +0200

    add checkmk example

---
+++ example/checkmk/checkmk.py
@@ -0,0 +1,89 @@
+import statsCollector
+from os import getenv, name as osname, path
+import sys
+import time
+import threading
+from socketserver import TCPServer,StreamRequestHandler
+import sqlite3
+
+nodeColor = False # Default Disable
+if str(getenv('Node64Color')).lower() == "true":
+    nodeColor=True
+
+if len(sys.argv) == 2:
+    nodeSecret = sys.argv[1]
+elif getenv('Node64Secret'):
+    nodeSecret = getenv('Node64Secret')
+else:
+    print(f"{sys.argv[0]} <your node secret>")
+    sys.exit(1)
+
+
+StopServer = False
+
+def runserver():
+    t = threading.current_thread()
+    while getattr(t, "do_run", True):
+        time.sleep(10)
+        print("Hello from Server")
+    print("Server Stop")
+
+class checkmk_checker(object):
+
+    def do_checks(self,**kwargs):
+        _lines = ["<<<check_mk>>>"]
+        _lines.append("AgentOS: node64")
+        _lines.append("Version: xy")
+        _lines.append("Hostname: node64")
+
+        con = sqlite3.connect(f"{path.dirname(path.realpath(__file__))}/stats.db")
+        fields = con.execute("SELECT field from tasks GROUP by field ORDER BY field ASC")
+        overallruntime = 0.0
+        overallcount = 0
+        _lines.append("<<<local:sep(0)>>>")
+        for field in fields:
+            sql = f"SELECT count(*) as count, SUM(runtime) as runtime from tasks WHERE field = '{field[0]}'"
+            #print(sql)
+            results = con.execute(sql)
+            data = results.fetchone()
+            count = data[0]
+            runtime = data[1] 
+            overallcount += count
+            overallruntime += runtime
+            _lines.append(f'0 "Task {field[0]}" count={count}|runtime={round(runtime,0)} {count} {field[0]} Tasks with a {round(runtime,0)}s runtime')
+        _lines.append(f'0 "node64 Tasks" count={overallcount}|runtime={round(overallruntime,0)} {overallcount} Tasks with a {round(overallruntime,0)}s runtime')
+        con.close()
+        return "\n".join(_lines).encode("utf-8")
+
+class checkmkHandler(StreamRequestHandler):
+    def handle(self):
+        try:
+            _msg = checkmk_checker.do_checks(self)
+        except Exception as e:
+            raise
+            _msg = str(e).encode("utf-8")
+        try:
+            self.wfile.write(_msg)
+        except:
+            pass
+
+def runserver():
+    
+    aServer = TCPServer(('', 6556), checkmkHandler)
+    aServer.serve_forever()
+
+if __name__ == "__main__":
+    if osname != 'nt':
+        from os import geteuid
+        if geteuid() != 0:
+            exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
+    print(f"check {__file__}")
+    threadserver = threading.Thread(target=runserver)
+    threadserver.start()
+    client = statsCollector.statsCollector(nodeSecret,nodeColor)
+    client.MaxWait = 16
+    client.run()
+    threadserver.do_run = False
+    #runserver()
+
+
---
+++ example/checkmk/statsCollector.py
@@ -0,0 +1,62 @@
+
+import sys
+from os import getenv, name as osname, path
+
+sys.path.append(path.dirname(path.dirname(path.dirname(path.realpath(__file__)))))
+
+from Node64Client import Node64Client
+
+import sqlite3
+import json
+
+class statsCollector(Node64Client):
+
+    def __init__(self, SecretKey, colorOutput=False):
+        super().__init__(SecretKey, colorOutput)
+        self.sqlinit()
+
+    def sqlinit(self):
+        self.con = sqlite3.connect(f"{path.dirname(path.realpath(__file__))}/stats.db",10)
+        self.con.execute('CREATE TABLE IF NOT EXISTS tasks (id CHAR PRIMARY KEY, field CHAR, successful INT, runtime REAL, dt datetime default current_timestamp);')
+        self.con.commit()
+        self.con.close()
+
+    def sqlwrite(self,task,result,runtime):
+        self.con = sqlite3.connect(f"{path.dirname(path.realpath(__file__))}/stats.db",10)
+        taskresult = 1
+        result = json.loads(result)
+
+        if 'error' in result:
+            if not result['error'] in {'0','no'}:
+                taskresult = 0
+        elif 'error_msg' in result:
+            if not result['error_msg'] in {'0','no'}:
+                taskresult = 0
+
+        self.con.execute(f"INSERT INTO tasks VALUES ('{task['task_id']}', '{task['task_type']}', {taskresult}, {runtime}, current_timestamp)")
+        self.con.commit()
+        self.con.close()
+
+    def stats(self,task,result,response,runtime):     
+        self.sqlwrite(task,result,runtime)
+
+if __name__ == "__main__":
+    if osname != 'nt':
+        from os import geteuid
+        if geteuid() != 0:
+            exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
+
+    nodeColor = False # Default Disable
+    if str(getenv('Node64Color')).lower() == "true":
+        nodeColor=True
+
+    if len(sys.argv) == 2:
+        nodeSecret = sys.argv[1]
+    elif getenv('Node64Secret'):
+        nodeSecret = getenv('Node64Secret')
+    else:
+        print(f"{sys.argv[0]} <your node secret>")
+        sys.exit(1)
+    client = statsCollector(nodeSecret,nodeColor)
+    client.MaxWait = 16
+    client.run()
